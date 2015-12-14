24138e4c189acc7609a297110fdbc35ce5ab21df
MarcBernat <marc.bernat@estudiant.upc.edu>
2015-12-14 13:21:49 +0100

    Readme quitados

--- service/src/main/java/edu/upc/eetac/dsa/videostore/Main.java
+++ service/src/main/java/edu/upc/eetac/dsa/videostore/Main.java
@@ -1,11 +1,11 @@
-package edu.upc.eetac.dsa.videostore;
-
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 /**
  * Main class.
@@ -13,20 +13,27 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    private static String baseURI;
 
+    public final static String getBaseURI() {
+        if (baseURI == null) {
+            PropertyResourceBundle prb = (PropertyResourceBundle) ResourceBundle.getBundle("beeter");
+            baseURI = prb.getString("beeter.context");
+        }
+        return baseURI;
+    }
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
-        // in edu.upc.eetac.dsa.videostore package
-        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.eetac.dsa.videostore");
+        // in edu.upc.eetac.dsa.beeter package
+        final ResourceConfig rc = new BeeterResourceConfig();
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(getBaseURI()), rc);
     }
 
     /**
@@ -37,9 +44,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+                + "%sapplication.wadl\nHit enter to stop it...", baseURI));
         System.in.read();
-        server.stop();
+        server.shutdownNow();
     }
 }
-
